5c29bc91b202b1a812aaa9a889788f4ccbf0bfc1
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-26 23:58:11 -0300

    adicionando requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,12 @@
 
 const myCounter = () => {
   let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
+  for (let i = 0; i <= 3; i += 1) {
+    myArray.push(i);
+    for (let j = 2; j <= 3; j += 1) {
+      myArray.push(j);
     }
   }
   return myArray;
 };
-
 module.exports = myCounter;
