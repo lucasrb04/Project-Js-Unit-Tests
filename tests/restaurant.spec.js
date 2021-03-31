613e1e6128b21c1d3462bb4f36890b0696d8ea55
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-31 11:46:36 -0300

    testando primeiros requisitos item 9

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -52,17 +52,20 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui,
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    assert.strictEqual(Object.keys(createMenu()).some((key) => key === 'fetchMenu'), true);
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`,
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
-    // const objetoRetornado = createMenu({ food: {}, drink: {} });
+    const objetoRetornado = createMenu({ food: {}, drink: {} });
+    console.log(Object.keys(objetoRetornado));
     // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
+    const exampleMenu = ['food', 'drink'];
+    // assert.deepStrictEqual( ,exampleMenu);
     // ```
     // TESTE 3: Verifique que o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
     // ```
